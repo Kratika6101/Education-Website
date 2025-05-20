ac837ba32f6c36324ba09cd42215dc9e555a9a2f
Kratika6101 <kratikarajput243@gmail.com>
2025-05-20 15:43:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,45 @@ window.addEventListener('scroll',()=>{
     /*classlist is a method to add,remove or check class of a div or nav*/
     /*toggle method removes a class from nav element on scroll*/
 })
+
+
+// show/hide faq answer //
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {  /*faqs is array andfaq is element*/
+    faq.addEventListener('click', ()=>{
+        faq.classList.toggle('open');
+    })
+
+    //change icon
+    const icon = faq.querySelector('.faq__icon i');
+    if (icon.className === 'uil uil-plus'){
+        icon.className = "uil uil-minus";
+    }else{
+        icon.className = "uil uil-plus";
+    
+    }
+})
+
+// SHOW/HIDE NAV MENU 
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// CLOSE NAV MENU 
+
+const closeNav = () =>{
+    menu.style.display ="none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);/*closenav is function*/
